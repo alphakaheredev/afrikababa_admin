f27d3ba45cec19d6b3671100db594c5a1e877c95
alphakaheredev <alpha.diallo@innovatelq.sn>
2024-08-08 16:25:07 +0000

    settings

--- src/pages/admin/Settings/Settings.tsx
+++ src/pages/admin/Settings/Settings.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList className="w-full justify-start bg-gray-100 space-x-4">
+        <TabsTrigger value="account">Moyens de paiements</TabsTrigger>
+        <TabsTrigger value="password">Condition</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        Make changes to your account here.
+      </TabsContent>
+      <TabsContent value="password">Change your password here.</TabsContent>
+    </Tabs>
+  );
 };
 
 export default Settings;
