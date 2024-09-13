f06709c481eb9cdaf2058131230f2f87c84522a5
alphakahere <alphakahere@gmail.com>
2024-09-13 02:08:22 +0000

    user tables

--- src/pages/admin/Customers/Customers.tsx
+++ src/pages/admin/Customers/Customers.tsx
@@ -1,23 +1,23 @@
 import { ButtonAddLink } from "@/components/ui/button";
-import CustomersTable from "./CustomersTable";
 import { InputSearch } from "@/components/ui/input";
 import { adminPaths } from "@/routes/paths";
+import UsersTable from "../Admins/UsersTable";
 
 const Customers = () => {
-  return (
-    <>
-      <div className="flex items-center justify-between mb-8">
-        <h3 className="text-dark font-semibold">Les clients</h3>
-        <div className="flex items-center justify-end gap-3 lg:w-2/3">
-          <InputSearch placeholder="Recherchez par nom" />
-          <ButtonAddLink to={adminPaths.addCustomer}>
-            Ajouter un client
-          </ButtonAddLink>
-        </div>
-      </div>
-      <CustomersTable />
-    </>
-  );
+	return (
+		<>
+			<div className="flex items-center justify-between mb-8">
+				<h3 className="text-dark font-semibold">Les clients</h3>
+				<div className="flex items-center justify-end gap-3 lg:w-2/3">
+					<InputSearch placeholder="Recherchez par nom" />
+					<ButtonAddLink to={adminPaths.addCustomer}>
+						Ajouter un client
+					</ButtonAddLink>
+				</div>
+			</div>
+			<UsersTable />
+		</>
+	);
 };
 
 export default Customers;
